cc4435cdb2a6f4207bbaa06bef5c9b6c07885f6e
Vinicius pereira <viniperirapg@gmail.com>
2022-08-31 18:35:37 -0300

    Mudando o redirecionamento de pagina

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
 function logado() {
   (function bemVindo() {
-    window.location.assign("http://127.0.0.1:5500/pagina-inicial.html");
+    window.location.assign(
+      "https://viniipereira.github.io/aluguel-de-carros-web-2/pagina-inicial.html"
+    );
   })();
 }
 
