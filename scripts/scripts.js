1a1001874c66d264a8eac96937ccd23b5935291c
Vinicius pereira <viniperirapg@gmail.com>
2022-08-31 21:37:29 -0300

    Adicionado estilo na pagina inicial e login

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,14 @@
-function logado() {
+const logado = () => {
   (function bemVindo() {
-    window.location.assign(
-      "https://viniipereira.github.io/aluguel-de-carros-web-2/pagina-inicial.html"
-    );
+    alert("Bem Vindo!");
   })();
-}
+};
+
+let login = (document.getElementById("login").onclick = function () {
+  (function paginaInicial() {
+    return (window.location.href = "http://127.0.0.1:5500/pagina-inicial.html");
+  })();
+});
 
 function duvida() {
   prompt("Envie a sua duvida!");
@@ -30,9 +34,12 @@ let relogio = () => {
 
   window.setTimeout("relogio()", 1000);
 };
+let color = () => {
+  return "#C78C94";
+};
 
 let mudarInput = function (obj) {
-  obj.style.backgroundColor = "#C78C94";
+  obj.style.backgroundColor = color();
 };
 
 function voltarInput(obj) {
@@ -43,10 +50,34 @@ function mouseSobreLogin(obj, color) {
   obj.style.backgroundColor = color;
 }
 
-let color = function () {
-  return "#C78C94";
-};
-
 function mouseForaLogin(obj) {
   obj.style.backgroundColor = "#FFFFFF";
+}
+
+function mudarCard(card) {
+  document.querySelector(card).style.backgroundColor = "#C76C81";
+  document.querySelector(card).style.transition = "2s";
+  document.querySelector(card).style.scale = "1.1";
+  console.log(document.querySelector(card).style);
+}
+
+function voltarCard(card) {
+  document.querySelector(card).style.backgroundColor = "#FFFFFF";
+  document.querySelector(card).style.scale = "1";
+}
+
+const listaDeCards = document.querySelectorAll(".card");
+
+for (let contador = 0; contador < listaDeCards.length; contador++) {
+  const card = listaDeCards[contador];
+  const sobreOCard = card.classList[1];
+  const idCard = `#${sobreOCard}`;
+
+  card.onmouseover = function () {
+    mudarCard(idCard);
+  };
+
+  card.onmouseout = function () {
+    voltarCard(idCard);
+  };
 }
